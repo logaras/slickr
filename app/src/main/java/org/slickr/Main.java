f898205e3f2a8c7b5efe2bd70575fd59fe0a28fa
logaras <logaras@gmail.com>
2014-09-10 20:47:22 +0300

    Main searchable activity doing the REST call.

---
+++ app/src/main/java/org/slickr/Main.java
@@ -0,0 +1,120 @@
+package org.slickr;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.BaseJsonHttpResponseHandler;
+
+import org.apache.http.Header;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+
+public class Main extends Activity {
+
+    ProgressBar progressBarView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayShowTitleEnabled(false);
+        setContentView(R.layout.activity_main);
+        progressBarView = (ProgressBar) findViewById(R.id.progressBar);
+        progressBarView.setVisibility(View.GONE);
+
+
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    /**
+     * @param intent
+     */
+    private void handleIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            Log.d(getString(R.string.app_name), "Searching for " + query);
+            String urlString = "";
+            try {
+                urlString = URLEncoder.encode(query, "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            Log.d(getString(R.string.app_name), Statics.FLICK_SEARCH_URL + urlString);
+
+
+            AsyncHttpClient client = new AsyncHttpClient();
+            client.get(Statics.FLICK_SEARCH_URL + urlString,
+                    new BaseJsonHttpResponseHandler() {
+
+                        @Override
+                        public void onSuccess(int statusCode, Header[] headers, String rawJsonResponse, Object response) {
+                            progressBarView.setVisibility(View.GONE);
+
+                            Log.d(getString(R.string.app_name), rawJsonResponse);
+                            try {
+                               JSONObject jsonObject = new JSONObject(rawJsonResponse);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(int statusCode, Header[] headers, Throwable throwable, String rawJsonData, Object errorResponse) {
+                            progressBarView.setVisibility(View.GONE);
+
+                            Log.e(getString(R.string.app_name), statusCode + "\n" + errorResponse);
+                        }
+
+                        @Override
+                        protected Object parseResponse(String rawJsonData, boolean isFailure) throws Throwable {
+                            return null;
+                        }
+
+                    });
+
+        }
+    }
+}
---
+++ app/src/main/java/org/slickr/Statics.java
@@ -0,0 +1,11 @@
+package org.slickr;
+
+/**
+ * Created by marlog on 9/10/14.
+ */
+public class Statics {
+    public static final String FLICKR_KEY = "a3e5933dec792ea59fd64d636716f4bc";
+    public static final String FLICKR_SECRET = "b2ddfb9f662f2f1b";
+    public static final String FLICK_BASE_URL = "https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&nojsoncallback=1";
+    public static final String FLICK_SEARCH_URL = FLICK_BASE_URL+"&api_key=a3e5933dec792ea59fd64d636716f4bc&text=";
+}
