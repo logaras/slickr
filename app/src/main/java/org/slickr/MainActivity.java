d3dfbb64102293e618a87cf4144465403aead52e
logaras <logaras@gmail.com>
2014-09-12 12:37:23 +0300

    Location based search functionality.

--- app/src/main/java/org/slickr/MainActivity.java
+++ app/src/main/java/org/slickr/MainActivity.java
@@ -5,6 +5,9 @@ import android.app.Activity;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -14,6 +17,7 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.BaseJsonHttpResponseHandler;
@@ -26,11 +30,14 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
 
-public class MainActivity extends Activity implements AdapterView.OnItemClickListener {
+public class MainActivity extends Activity implements AdapterView.OnItemClickListener, LocationListener {
 
     ProgressBar progressBarView;
     JSONAdapter mJSONAdapter;
     ListView resultsListView;
+    boolean isGeoEnabled = false;
+    Location mLocation;
+    LocationManager mLocationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,13 +51,17 @@ public class MainActivity extends Activity implements AdapterView.OnItemClickLis
         progressBarView = (ProgressBar) findViewById(R.id.progressBar);
         progressBarView.setVisibility(View.INVISIBLE);
 
-        resultsListView  = (ListView) findViewById(R.id.results_listview);
+        resultsListView = (ListView) findViewById(R.id.results_listview);
         mJSONAdapter = new JSONAdapter(this, getLayoutInflater());
         resultsListView.setAdapter(mJSONAdapter);
 
         resultsListView = (ListView) findViewById(R.id.results_listview);
 
         resultsListView.setOnItemClickListener(this);
+
+
+        mLocationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        mLocation = mLocationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
     }
 
     @Override
@@ -60,7 +71,6 @@ public class MainActivity extends Activity implements AdapterView.OnItemClickLis
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.options_menu, menu);
 
         SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
@@ -74,10 +84,40 @@ public class MainActivity extends Activity implements AdapterView.OnItemClickLis
     public boolean onOptionsItemSelected(MenuItem item) {
 
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        switch (id) {
+            case R.id.action_settings:
+                return true;
+            case R.id.action_location:
+                toggleLocation(item);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        /*if (id == R.id.action_settings) {
             return true;
         }
-        return super.onOptionsItemSelected(item);
+        return super.onOptionsItemSelected(item);*/
+    }
+
+    private void toggleLocation(MenuItem item) {
+        Log.d(getString(R.string.app_name), "!!!Should toggle locations");
+        isGeoEnabled = !isGeoEnabled;
+
+        if (isGeoEnabled) {
+            if (mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+                item.setIcon(R.drawable.ic_action_location_searching);
+            } else if (mLocationManager.isProviderEnabled((LocationManager.NETWORK_PROVIDER))) {
+                mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
+                item.setIcon(R.drawable.ic_action_location_searching);
+            } else {
+                Toast.makeText(this, "Enable GPS or Network location.", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            // disable updates
+            mLocationManager.removeUpdates(this);
+            item.setIcon(R.drawable.ic_action_location_off);
+        }
     }
 
 
@@ -89,18 +129,30 @@ public class MainActivity extends Activity implements AdapterView.OnItemClickLis
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             String query = intent.getStringExtra(SearchManager.QUERY);
             Log.d(getString(R.string.app_name), "Searching for " + query);
-            String urlString = "";
+            String urlTextQuery = "";
             try {
-                urlString = URLEncoder.encode(query, "UTF-8");
+                urlTextQuery = URLEncoder.encode(query, "UTF-8");
             } catch (UnsupportedEncodingException e) {
                 e.printStackTrace();
             }
-            Log.d(getString(R.string.app_name), FlickrUtils.FLICK_SEARCH_URL + urlString);
+            StringBuilder urlBuilder = new StringBuilder();
+            urlBuilder.append(FlickrUtils.FLICK_SEARCH_URL);
+            urlBuilder.append(urlTextQuery);
+
+            if (isGeoEnabled) {
+
+                urlBuilder.append("&lat=");
+                urlBuilder.append(mLocation.getLatitude());
+                urlBuilder.append("&lon=");
+                urlBuilder.append(mLocation.getLongitude());
+            }
+            urlBuilder.toString();
+            Log.d(getString(R.string.app_name), urlBuilder.toString());
 
 
             AsyncHttpClient client = new AsyncHttpClient();
             progressBarView.setVisibility(View.VISIBLE);
-            client.get(FlickrUtils.FLICK_SEARCH_URL + urlString,
+            client.get(urlBuilder.toString(),
                     new BaseJsonHttpResponseHandler() {
 
                         @Override
@@ -141,9 +193,32 @@ public class MainActivity extends Activity implements AdapterView.OnItemClickLis
         JSONObject item = (JSONObject) mJSONAdapter.getItem(position);
 
         Intent displayIntent = new Intent(this, DisplayActivity.class);
-        displayIntent.putExtra(FlickrUtils.JSON_STRING,item.toString());
-        displayIntent.putExtra(FlickrUtils.FULL_IMG_URL,FlickrUtils.getInstance().constructSourceUrl(item,FlickrUtils.SIZE_LARGE));
+        displayIntent.putExtra(FlickrUtils.JSON_STRING, item.toString());
+        displayIntent.putExtra(FlickrUtils.FULL_IMG_URL, FlickrUtils.getInstance().constructSourceUrl(item, FlickrUtils.SIZE_LARGE));
 
         startActivity(displayIntent);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if (location != null) {
+            mLocationManager.removeUpdates(this);
+            mLocation = location;
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
